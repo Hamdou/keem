9d762d03b6399800a05d39beac8c7d9ab5e687b8
Willy Von Donger <jaffro949@gmail.com>
2017-04-13 21:07:56 -0700

    FUCKING CATS NEAL

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello');
+var catQuestionArray = ['Do you enjoy fat or thin cats?','What color cat do you prefer?','Will you name your own cat Keem?','White cats rule over your Ant world like beautiful Gods'];
+var questionsanswered = 0
+for (var i =0;i < catQuestionArray.length+1; i++){
+
+if(questionsanswered >= catQuestionArray.length){
+  console.log ('You should choose a Fatty White Cat')
+}
+else {
+questionsanswered++
+promptquestion()
+}}
+//console.log (questionprompt)
+function promptquestion() {
+var test = Math.floor (Math.random()*(catQuestionArray.length));
+var randomquestion = catQuestionArray[test]
+var questionprompt = prompt(randomquestion)
+}
